96a56b5b08f42da56f45b8c66252dccf9ec648a8
viktorchergutsa <viktorchergutsa@yahoo.com>
2020-09-26 20:33:15 -0700

    Added code for favorite animal

--- animal.py
+++ animal.py
@@ -4,4 +4,4 @@
 #              prints out that that is their favorite animal
 
 fave_animal = input("Please enter your favorite animal: ")
-print("Your  favorite animal is the " + fave_animal + ".")
+print("Your favorite animal is the " + fave_animal + ".")
